1ebec9ad9ac5eaec26f1f99331ba450bd3f24752
Andrew <telepnev.1997@mail.ru>
2024-03-09 10:11:49 +0700

    change eureka base uri

--- ClothingStoreResourceServer/src/main/java/com/clothingstore/ClothingStoreResourceServer/ClothingStoreResourceServerApplication.java
+++ ClothingStoreResourceServer/src/main/java/com/clothingstore/ClothingStoreResourceServer/ClothingStoreResourceServerApplication.java
@@ -2,8 +2,10 @@ package com.clothingstore.ClothingStoreResourceServer;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class ClothingStoreResourceServerApplication {
 
 	public static void main(String[] args) {
